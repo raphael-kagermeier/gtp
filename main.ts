75265cfa43cdfbae2bf0c1bc8e3fb2f8fe372b1e
raphael <r.kagermeier@performromance.com>
2023-01-19 00:05:30 +0100

    first build

--- main.ts
+++ main.ts
@@ -1,85 +1,38 @@
 import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import fetch from "node-fetch";
+
 
 // Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+interface  PluginSettings {
+	apiKey: string;
+	organisationId: string;
+	defaultMaxTokenLength: number;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: PluginSettings = {
+	apiKey: '',
+	organisationId: '',
+	defaultMaxTokenLength: 50,
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class Gtp extends Plugin {
+	settings: PluginSettings;
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
 		// This adds an editor command that can perform some operation on the current editor instance
 		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
+			id: 'gtp',
+			name: 'Run ChatGTP',
 			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
+				const onResponse = (response:string) => {editor.replaceSelection(response)};
+				new DialogModal(this.app, onResponse,this.settings).open();
 			}
 		});
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
-
-	onunload() {
-
+		this.addSettingTab(new SettingTab(this.app, this));
 	}
 
 	async loadSettings() {
@@ -91,14 +44,73 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
+// set return type to string
+class DialogModal extends Modal{
+
+	onResponse: (response:any)  => any;
+	settings: PluginSettings;
+	maxTokens:number;
+
+	constructor(
+		app: App,
+		onResponse: (response:any) => any,
+		settings: PluginSettings
+	) {
 		super(app);
+		this.onResponse = onResponse;
+		this.settings = settings;
 	}
 
 	onOpen() {
 		const {contentEl} = this;
-		contentEl.setText('Woah!');
+		contentEl.createEl('h2', {text: 'Run ChatGTP'});
+
+		// text input
+		let inputPrompt = contentEl.createEl('textarea', {placeholder: 'Enter your GTP command here', cls: 'gtp_prompt_input'});
+		/*inputPrompt.addEventListener('keydown', (evt) => {
+			if (evt.key === 'Enter') {
+				this.runCommand(inputPrompt.value).then((response:any) => {
+					this.onResponse(response['choices'][0].text);
+				});
+			}
+		});*/
+
+		// button
+		contentEl.createEl('button', {text: 'Run', cls: 'gtp_run_button'}).addEventListener('click', () => {
+			this.runCommand(inputPrompt.value).then((response:any) => {
+				this.onResponse(response['choices'][0].text);
+			});
+		});
+
+	}
+
+	async runCommand(command:string) {
+		// show a notice that the command is running
+		new Notice('Running command: ' + command);
+
+		const response = await fetch("https://api.openai.com/v1/completions", {
+			method: 'POST',
+			headers: {
+				"Content-Type": "application/json",
+				"OpenAI-Organization": this.settings.organisationId,
+				"Authorization": "Bearer " + this.settings.apiKey
+			},
+			body: JSON.stringify({
+				prompt: command + 'in markdown',
+				model: "text-davinci-003",
+				"temperature": 0,
+				"max_tokens": Number(this.settings.defaultMaxTokenLength),
+			}),
+		} );
+
+		// TODO error handling
+		if (!response.ok) {
+			return new Notice('error:  ' + response.statusText);
+		}
+
+		// close the modal
+		this.close();
+		return response.json();
 	}
 
 	onClose() {
@@ -107,10 +119,10 @@ class SampleModal extends Modal {
 	}
 }
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+class SettingTab extends PluginSettingTab {
+	plugin: Gtp;
 
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: Gtp) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
@@ -120,17 +132,38 @@ class SampleSettingTab extends PluginSettingTab {
 
 		containerEl.empty();
 
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
+		containerEl.createEl('h2', {text: 'Open AI Credentials'});
 
 		new Setting(containerEl)
-			.setName('Setting #1')
+			.setName('Open AI API key')
 			.setDesc('It\'s a secret')
 			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+				.setPlaceholder('Enter your API key')
+				.setValue(this.plugin.settings.apiKey)
 				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
+					this.plugin.settings.apiKey = value;
+					await this.plugin.saveSettings();
+				}));
+
+		new Setting(containerEl)
+			.setName('Organisation ID')
+			.setDesc('It\'s a secret too')
+			.addText(text => text
+				.setPlaceholder('Enter your Organisation id')
+				.setValue(this.plugin.settings.organisationId)
+				.onChange(async (value) => {
+					this.plugin.settings.organisationId = value;
+					await this.plugin.saveSettings();
+				}));
+
+		new Setting(containerEl)
+			.setName('Max Tokens')
+			.setDesc('This will be the max number of tokens returned by the API')
+			.addText(text => text
+				.setPlaceholder('Enter your max tokens')
+				.setValue(String(this.plugin.settings.defaultMaxTokenLength))
+				.onChange(async (value) => {
+					this.plugin.settings.defaultMaxTokenLength = Number(value);
 					await this.plugin.saveSettings();
 				}));
 	}
